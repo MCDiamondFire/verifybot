e25e559d6e6e00ea29b020a552f469776ba30087
LittleWhole <wholelittle@gmail.com>
2018-03-26 11:31:42 -0400

    Update contribute.js

--- commands/information/contribute.js
+++ commands/information/contribute.js
@@ -11,8 +11,6 @@ module.exports = class Pick extends Base {
         });
     }
 
-    run(message, args) {
-        if (args === null) {
+    run(message) {
              message.channel.send("Hi! Thanks for contributing! If you'd like to know more about contributing, visit the VerifyBot official GitHub repository at https://github.com/RedstoneDaedalus/verifybot/ ! Contribution guidelines, code of conduct, and pull request template are all on there. If you'd like to learn about pull requests, visit https://yangsu.github.io/pull-request-tutorial/ . Thanks for contributing! :confetti_ball:");
-    }
 };
