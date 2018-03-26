df7e2464cdbbec2a4da12587958243c06539c37b
Dragzii <37547804+Dragzii@users.noreply.github.com>
2018-03-26 08:31:16 -0700

    !random

---
+++ commands/fun/random.js
@@ -0,0 +1,25 @@
+const Base = require("../../base/Command.js");
+
+module.exports = class Hack extends Base {
+    constructor(client) {
+        super(client, {
+            name: "random",
+            description: "Generates a random number.",
+            usage: "",
+            category: "fun",
+            permLevel: 0 
+        });
+    }
+
+    async run(message, args) {
+        // Ignore if sent in dfchat
+        if (message.channel.name === "dfchat") return super.error("You can't use that here!");
+        
+        const rnd = Math.random()
+          Math.floor()
+    }
+
+    pause() {
+        return new rnd(r => setTimeout(r, 5000));
+    }
+};
